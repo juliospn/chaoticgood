295a0dd3b4cb8aeb0b4e32f2bb512cd0d17ffe35
juliospn <116590563+juliospn@users.noreply.github.com>
2023-01-11 20:52:48 -0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 function goToRandomArticle() {
   // Crie uma lista de URLs para os seus artigos
-  const articles = ['juliospn/caothicgood/filosofos/karlmarx.html', '/aynrand.html', '/article-3'];
+  const articles = ['./filosofos/karlmarx.html', './filosofos/aynrand.html', './filosofos/hans-herman-hoppe.html','./filosofos/gustave-de-molinari.html','./filosofos/lysander-spooner.html','./filosofos/friedrich-hayek.html','./artigos/anarquismo-espiritualidade.html','./artigos/anomia-anarquia.html','./artigos/cypher-nao-pise-em-mim.html','./artigos/nakamoto-bitcoin.html'];
 
   // Sorteie um número aleatório entre 0 e o tamanho da lista de artigos
   const randomIndex = Math.floor(Math.random() * articles.length);
